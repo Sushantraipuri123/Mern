e10d2f2dbc2a2f12f1ace45271bc8b468580a789
Ashu <cashu853@gmail.com>
2024-07-30 09:13:27 +0530

    zod validator

--- registering user/backend/routes/users.js
+++ registering user/backend/routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const signUpSchema = require('../validstor/auth-validator')
+const valiidate = require('../middleware/validator-middleware')
 
 var authControler = require('../controllers/authController')
 /* GET users listing. */
-router.post('/createUser',authControler.createUser);
+
+router.post('/createUser',valiidate(signUpSchema), authControler.createUser);
 
 router.get('/getUser',authControler.getUser);
 
