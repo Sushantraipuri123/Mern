7962ee91dd31b1a5fc3137e08701885af44fc62b
Ashu <cashu853@gmail.com>
2024-08-04 16:33:18 +0530

    done with created user

--- auth test/backend/controllers/authController.js
+++ auth test/backend/controllers/authController.js
@@ -1,8 +1,19 @@
 var db = require('../models/user.model');
+const bcrypt = require('bcrypt');
 
 module.exports ={
     createUser : async (req,res)=>{
         try {
+
+            const exsistingUser = await db.findOne({email:req.body.email});            
+
+            if(exsistingUser){
+                return res.status(400).json({
+                    success:false,
+                    status:400,
+                    message:"Email already exists",
+                })
+            }
            
             const user = await db.create({
                 username:req.body.username,
@@ -15,7 +26,9 @@ module.exports ={
                 success:true,
                 status:200,
                 message:"User Created ",
-                body:user
+                body:user,
+                token : await user.generateToken(),
+                userId : user._id.toString()
             })
 
         } catch (error) {
--- auth test/backend/models/user.model.js
+++ auth test/backend/models/user.model.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema(
     {
@@ -25,5 +27,39 @@ const userSchema = new mongoose.Schema(
     },{
         timestamps: true
     })
+
+        // ===============used pre function as a middlewere function to hash my pass word 
+    
+        userSchema.pre('save', async function(next){
+            console.log('pre data', this);
+     
+            if(!this.isModified('password')){
+                 return next();
+            }
+     
+            const user = this;
+            const saltRounds = 10;
+            const hashedPsssword = await bcrypt.hash(user.password , saltRounds);
+            user.password = hashedPsssword;
+         })
+
+          // ===============used pre function as a middlewere function to generate jwt tokken
+
+   userSchema.methods.generateToken = async function(){
+    try {
+        return jwt.sign({
+            userID: this._id.toString(),
+            email: this.email,
+            isAdmin: this.isAdmin
+        },
+    process.env.JWT_SECRET_KEY,
+        { expiresIn: '30d' }  //  expiry time
+    )
+    } catch (error) {
+        console.log(error);
+        throw new Error('Failed to generate token')
+    }
+   }
+
 
 module.exports = mongoose.model('User', userSchema);    
