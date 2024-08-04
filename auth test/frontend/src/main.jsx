5cb10acb793884e894af2264c0f5e033ff9c6196
Ashu <cashu853@gmail.com>
2024-08-04 17:06:18 +0530

    done with routing

--- auth test/frontend/src/main.jsx
+++ auth test/frontend/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
+import Layout from './Layout.jsx';
+import Home from './components/Home.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+ <Router>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>} />
+         
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
